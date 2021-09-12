cb90877e13ca0d9ba3e3700f82e38f6a66d92a95
Luiz Carlos Vartuli <37045052+ziul123@users.noreply.github.com>
2021-09-12 12:22:17 -0300

    Add -h option

--- pgit.py
+++ pgit.py
@@ -1,11 +1,22 @@
+"""Fills out git username and token recovered from Bitwarden CLI.
+User is prompted for Bitwarden master password. Locks session immediately after recovering token.
+Use "bw list items --pretty" to find the Bitwarden item ID for the github token.
 
+Usage
+-----
+pgit pull
+pgit push
+"""
 
 if __name__ == '__main__':
 	import subprocess as sb
 	import sys, json
 
 	git_token_id = "TOKEN_BITWARDEN_ITEM_ID"
-	if sys.argv[1] == "push":
+	if sys.argv[1] == '-h' or sys.argv[1] == '--help':
+			print(__doc__)
+			quit()
+	elif sys.argv[1] == "push":
 		cmd = "git push"
 	elif sys.argv[1] == "pull":
 		cmd = "git pull"
